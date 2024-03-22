15252572e54750e5dd9329b328ee52b7de7f15c3
Uğur Okan ÇIVGİN <uocivgin@isbak.istanbul>
2024-03-22 15:58:46 +0300

    .

--- Order.API/Consumers/OrderCompletedEventConsumer.cs
+++ Order.API/Consumers/OrderCompletedEventConsumer.cs
@@ -8,7 +8,7 @@ namespace Order.API.Consumers
     {
         public async Task Consume(ConsumeContext<OrderCompletedEvent> context)
         {
-            Order.API.Models.Order order = await orderDbContext.Orders.FindAsync(context.Message.OrderId);
+            Models.Order order = await orderDbContext.Orders.FindAsync(context.Message.OrderId);
             if (order != null)
             {
                 order.OrderStatus = Enums.OrderStatus.Completed;
